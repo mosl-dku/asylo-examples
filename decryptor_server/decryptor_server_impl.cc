913377fc053ec5a226357e16eb9e844aecb38f45
Seehwan Yoo <seehwan.yoo@dankook.ac.kr>
2021-02-10 19:03:51 +0900

    [Bug fixed]error while decrypting

--- decryptor_server/decryptor_server_impl.cc
+++ decryptor_server/decryptor_server_impl.cc
@@ -15,44 +15,273 @@
  * limitations under the License.
  *
  */
+#include <cstdint>
 
-#include "decryptor_server/decryptor_server_impl.h"
+#include <atomic>
+#include <cstddef>
+#include <string>
+#include <vector>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <openssl/aead.h>
+#include <openssl/bio.h>
+#include <openssl/rsa.h>
+#include <openssl/evp.h>
+#include <openssl/pem.h>
+#include <openssl/x509.h>
+
+#include "asylo/examples/decryptor_server/decryptor_server_impl.h"
 
 #include "absl/strings/ascii.h"
 #include "absl/strings/str_cat.h"
 #include "include/grpcpp/grpcpp.h"
+#include "asylo/crypto/algorithms.pb.h"
+#include "asylo/crypto/util/bssl_util.h"
+#include "asylo/util/cleanup.h"
+#include "asylo/util/status_macros.h"
+#include "asylo/crypto/aead_key.h"
+#include "zlib.h"
+
+#include "absl/memory/memory.h"
+#include "absl/strings/str_cat.h"
+
 
 namespace examples {
 namespace decryptor_server {
 
-DecryptorServerImpl::DecryptorServerImpl()
-    : Service(),
-      // Initialize the translation map with a few known translations.
-      translation_map_({{"asylo", "sanctuary"},
-                        {"istio", "sail"},
-                        {"kubernetes", "helmsman"}}) {}
 
-::grpc::Status DecryptorServerImpl::GetDecryption(
+const char AssociatedDataBuf[] = "";
+
+std::string ReplaceAll(std::string str, const std::string& from, const std::string& to) {
+        size_t start_pos = 0;
+        while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+                str.replace(start_pos, from.length(), to);
+                start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
+        }
+        return str;
+}
+
+
+uint8_t* RetriveKeyFromString(std::string stf,size_t key_size){
+        uint8_t *key=new uint8_t[key_size];
+        unsigned long ul;
+        char *dummy;
+
+        for(int i=0; i<key_size; ){
+                ul = strtoul(stf.substr( i*2, 8).c_str(), &dummy, 16);
+                key[i++] = (ul & 0xff000000)>>24;
+                key[i++] = (ul & 0xff0000)>>16;
+                key[i++] = (ul & 0xff00)>>8;
+                key[i++] = (ul & 0xff);
+        }
+
+        return key;
+}
+
+/*
+	Retrive Key decryption Key from private key file
+*/
+RSA *GetKDK(const char *private_key_file)
+{
+	struct stat sbuf;
+    BIO *keybio = NULL;
+    RSA *output = NULL;
+	int res = stat(private_key_file, &sbuf);
+	if (res == 0) {
+		// works only when the cert_file exists
+		keybio = BIO_new(BIO_s_file());
+		BIO_read_filename(keybio, private_key_file);
+		output = PEM_read_bio_RSAPrivateKey(keybio, NULL, NULL, NULL);
+		BIO_free(keybio);
+	}
+
+	if (output == NULL) {
+		LOG(ERROR) << "GetKDK failed";
+		return NULL;
+	}
+	LOG(INFO) << "[GetKDK]: "<< output;
+    return output;
+}
+
+/*
+	Read encrypted key-decryption-key
+	input: key_file
+		the filename for the encrypted key
+			out_length
+		output param for key length
+	output: keybytes
+	N.B: the caller should free the keybytes
+*/
+uint8_t *ReadEncKey(const char *key_file, int *out_length)
+{
+	struct stat sbuf;
+	uint8_t *p = NULL;
+    BIO *inputbio = NULL;
+	int nrbytes = 0;
+	int res = stat(key_file, &sbuf);
+	if (res == 0) {
+		// works only when the cert_file exists
+		p = new uint8_t[256];
+		inputbio = BIO_new(BIO_s_file());
+		BIO_read_filename(inputbio, key_file);
+		nrbytes = BIO_read(inputbio, p, 256);
+	}
+
+	if (nrbytes <= 0) {
+		LOG(ERROR) << "ReadEncKey failed";
+		return NULL;
+	}
+	LOG(INFO) << "[enc_key]: "<< p;
+	*out_length = nrbytes;
+	return p;
+}
+
+/*
+	Decrypt Data encryption key
+		from the encrypted_key (enc_key) and certificate (key-decryption-key)
+	input:  byte[] enc_key
+				encrypted key from file,
+			int enc_key_len
+				key length of enc_key
+			RSA* Kpub
+				key-decryption-key from certificate
+	output: byte[] dek
+				data encryption key
+				the caller should free the dek
+*/
+uint8_t *DecryptDEK(uint8_t *enc_key, int klen, RSA *Kpriv)
+{
+	uint8_t str_key[256];
+	int key_length;
+	int dek_length = 32;
+	uint8_t *dek;
+	memset(str_key, 0, 256);
+	key_length = RSA_private_decrypt(klen, enc_key, str_key, Kpriv, RSA_PKCS1_PADDING);
+	std::string input_key((char *)str_key);
+
+	if (key_length <= 0) {
+		LOG(ERROR) << "DecryptDEK failed";
+		return NULL;
+	}
+	LOG(INFO) << "[AES-GCM key]: "<< input_key << "\nkeylen: " << key_length;
+
+	dek = RetriveKeyFromString(ReplaceAll(input_key, std::string(" "), std::string("")), dek_length);
+	return dek;
+}
+
+/*
+	Decrypt And Decompress
+		The plaindata is compressed and then encrypted
+	input: 	std::string cipher_text
+				the ciphertext (original data)
+			uint8_t* key
+				the ciphering key
+*/
+std::string DecryptAndDecompress(std::string &cipher_text, uint8_t *key)
+{
+	int data_len = cipher_text.length();
+	size_t out_len;
+	uint8_t* dout = new uint8_t[data_len];
+	uint8_t nonce[32] = {0,};
+
+	EVP_AEAD_CTX ctx;
+    const EVP_AEAD *const aead = EVP_aead_aes_256_gcm();
+    size_t nonce_len= EVP_AEAD_nonce_length(aead);
+    EVP_AEAD_CTX_init(&ctx, aead, key, EVP_AEAD_key_length(aead),EVP_AEAD_DEFAULT_TAG_LENGTH, NULL);
+    EVP_AEAD_CTX_open(&ctx, dout, &out_len, data_len, nonce, nonce_len, (const uint8_t *)(cipher_text.c_str()), data_len,NULL, 0);
+	if (out_len == 0) {
+		delete dout;
+		return std::string();
+	}
+    LOG(INFO) << "[DEBUG] Decrypted Data ("<< out_len <<"): "<< dout;
+
+    // decompress
+    unsigned char* pCompressedData = (unsigned char*) dout;
+    unsigned long out_buffer_length = (out_len << 4);
+    unsigned char * pUncompressedData = new unsigned char [out_buffer_length];
+    if (pCompressedData != nullptr) memset(pUncompressedData,0,out_buffer_length);
+    int nResult = uncompress(pUncompressedData, &out_buffer_length, dout, out_len);
+    if(nResult != Z_OK) {
+		delete dout;
+		delete pUncompressedData;
+		return std::string();
+	}
+    //LOG(INFO) << "[DEBUG] Decrypted and Uncompressed Data ("<< out_buffer_length <<"): " << pUncompressedData;
+
+	//std::string out((char *)pUncompressedData, out_len);
+	std::string out((char *)pUncompressedData);
+	delete dout;
+	delete pUncompressedData;
+	//LOG(INFO) << "[DEBUG] Decrypted and Uncompressed Data (" << out;
+	return out;
+}
+
+DecryptorServerImpl::DecryptorServerImpl()
+    : Service()
+{}
+
+::grpc::Status DecryptorServerImpl::Decrypt(
     ::grpc::ServerContext *context, const GetDecryptionRequest *request,
     GetDecryptionResponse *response) {
-  // Confirm that |*request| has an |input_word| field.
-  if (!request->has_input_word()) {
-    return ::grpc::Status(::grpc::StatusCode::INVALID_ARGUMENT,
-                          "No input word given");
-  }
+	// To decrypt data, 
+	/*
+		1. extract the key decryption key from certificate
+		2. read encrypted key file
+		3. decrypt the data encryption key (2) with the key decryption key (1)
+		4. read the encrypted data (ciphertext in the request)
+		5. decrypt data (4) with the key decryption key (3)
+	*/
+	RSA *privkey; // key decryption key
+	uint8_t *dek; // data encryption key
+	uint8_t *enc_key;
+	int enc_key_len;
 
-  // Confirm that the translation map has a translation for the input word.
-  auto response_iterator =
-      translation_map_.find(absl::AsciiStrToLower(request->input_word()));
-  if (response_iterator == translation_map_.end()) {
-    return ::grpc::Status(::grpc::StatusCode::INVALID_ARGUMENT,
-                          absl::StrCat("No known translation for \"",
-                                       request->input_word(), "\""));
-  }
+	// Confirm that |*request| has an |ciphertext| field.
+	if (!request->has_ciphertext()) {
+		return ::grpc::Status(::grpc::StatusCode::INVALID_ARGUMENT,
+			"No input ciphertext given");
+	}
+	// Confirm that |*request| has an |ciphertext| field.
+	if (!request->has_key_filename()) {
+		return ::grpc::Status(::grpc::StatusCode::INVALID_ARGUMENT,
+			"No input key_filename given");
+	}
 
-  // Return the translation.
-  response->set_translated_word(response_iterator->second);
-  return ::grpc::Status::OK;
+
+
+	char priv_key_file[] = "/key_material/private.key";
+	char *encrypted_key_filename = (char *)request->key_filename().c_str();
+	// Check the enc_key file: encrypted key
+	privkey = GetKDK(priv_key_file);
+	if (privkey == NULL) {
+		return ::grpc::Status(::grpc::StatusCode::INVALID_ARGUMENT,
+								"No valid certificate file available");
+	}
+	enc_key = ReadEncKey(encrypted_key_filename, &enc_key_len);
+	if (enc_key == NULL) {
+		return ::grpc::Status(::grpc::StatusCode::INVALID_ARGUMENT,
+								"No valid key file available");
+	}
+
+	dek = DecryptDEK(enc_key, enc_key_len, privkey);
+	if (dek == NULL) {
+		return ::grpc::Status(::grpc::StatusCode::INVALID_ARGUMENT,
+								"decryption key derivation failed");
+	}
+	RSA_free((RSA *)privkey);
+
+	std::string cipher_text = request->ciphertext();
+	std::string plaintext = DecryptAndDecompress(cipher_text, dek);
+	if (plaintext.empty()) {
+		return ::grpc::Status(::grpc::StatusCode::INVALID_ARGUMENT,
+                          "Decryption failed");
+	}
+
+	// Return the plaintext.
+	response->set_plaintext(plaintext);
+
+	delete enc_key;
+	return ::grpc::Status::OK;
 }
 
 }  // namespace decryptor_server
